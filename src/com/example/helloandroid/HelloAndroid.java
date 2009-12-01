27b996d2b66bead744a94569b2dd9a45b0767602
Tomasz Stachewicz <tomasz.stachewicz@aenima.pl>
2009-12-01 18:00:36 +0100

    populating spinner with array from xml and pointing array adapter

--- src/com/example/helloandroid/HelloAndroid.java
+++ src/com/example/helloandroid/HelloAndroid.java
@@ -2,6 +2,8 @@ package com.example.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class HelloAndroid extends Activity {
@@ -10,5 +12,12 @@ public class HelloAndroid extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Spinner shape_chooser = (Spinner) findViewById(R.id.spinner_wat);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+        		this, R.array.shapes, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    shape_chooser.setAdapter(adapter);
+	    
     }
 }
